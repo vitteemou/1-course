5845a12b17df1f81f1f34d550d069f196b63cb37
vitteemou <vitteemou@gmail.com>
2015-11-18 01:08:14 +0300

    Update stack.h
    Code style corrections in stack.h file

--- 14_2_task/14_2_task/stack.h
+++ 14_2_task/14_2_task/stack.h
@@ -1,30 +1,21 @@
-/*Реализовать стек с дополнительной функцией value_type& min() как адаптер над обычным стеком, в качестве которого может выступать любой стек,
-а не только std::stack. Функция min() должна возвращать ссылку на  минимальный элемент в стеке, для пустого стека ее значение не определено.
-Функции push, pop, top и min должны работать за O(1). Сигнатура класса должна быть следующей:
-template<class T, class C = std::stack<T> > class Stack*/
-
 #include <stack>
 
 template<class T, class C = std::stack < T > >
-class Stack
-{
-
+class Stack {
 public:
-
    typedef typename T value_type;
    typedef typename C::size_type size_type;
 
    Stack();
    explicit Stack(const Stack&);
-
+   
    bool empty()
    {
-	   return stack.empty();
+        return stack.empty();
    }
-
    size_type size()
    {
-	   return stack.size();
+	return stack.size();
    }
 
    void push(const value_type&);
@@ -57,13 +48,16 @@ template<class T, class C>
 void Stack<T, C>::push(const value_type& x)
 {
    if(!stack.empty())
-		{
-		   if(stack.top() >= x)
-			   min_stack.push(x);
-		}
+   {
+	if(stack.top() >= x)
+	{
+              min_stack.push(x);
+	}
+   }
    else
+   {
        min_stack.push(x);
-
+   }
    stack.push(x);
 }
 
@@ -71,17 +65,20 @@ template<class T, class C>
 void Stack<T, C>::pop()
 {
    	if(!stack.empty())
+	{
+	        if(stack.top()==min_stack.top())
 		{
-			if(stack.top()==min_stack.top())
-				min_stack.pop();
-
-			stack.pop();
+		       min_stack.pop();
 		}
+  	        stack.pop();
+	}
 }
 
 template<class T, class C>
 const typename Stack<T, C>::value_type& Stack<T, C>::min()
 {
    	if(!stack.empty())
+   	{
 		return min_stack.top();
+   	}
 }
